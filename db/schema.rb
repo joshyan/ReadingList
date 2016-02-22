d9b904650a8b09fec8175218699bc598b5946914
Josh Yan <joshyansmile@gmail.com>
2016-02-21 19:22:03 -0800

    Add keywords to Search function.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222011053) do
+ActiveRecord::Schema.define(version: 20160222031407) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160222011053) do
     t.datetime "updated_at",  null: false
     t.integer  "rating"
     t.date     "finished_on"
+    t.string   "keywords"
   end
 
 end
