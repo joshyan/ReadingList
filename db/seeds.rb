cc877587287067588e9dbebd147d795bcff6b85a
Josh Yan <joshyansmile@gmail.com>
2016-02-21 18:00:27 -0800

    Add finished and recent to Book model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create!([{
+  title: "The 7 Habits of Highly Effective People: Powerful Lessons in Personal Change",
+  author: "Stephen R. Covey",
+  description: "One of the most inspiring and impactful books ever written, The 7 Habits of Highly Effective People has captivated readers for 25 years. It has transformed the lives of Presidents and CEOs, educators and parents— in short, millions of people of all ages and occupations.",
+  amazon_id: "1451639619",
+  rating: 5,
+  finished_on: 10.days.ago
+},
+{
+  title: "Harry Potter and the Cursed Child , Parts I & II",
+  author: "J.K. Rowling",
+  description: "Based on an original new story by J.K. Rowling, Jack Thorne and John Tiffany, a new play by Jack Thorne, Harry Potter and the Cursed Child is the eighth story in the Harry Potter series and the first official Harry Potter story to be presented on stage. The play will receive its world premiere in London’s West End on July 30, 2016.",
+  amazon_id: "1338099132",
+  rating: 4,
+  finished_on: 1.day.ago
+}])
